bd81c5ca5b0afcfe181b048ab160179bfea7ca1c
Wassim DHIF <wassim.dhif@datadoghq.com>
2023-11-29 11:06:24 +0100

    [test/new-e2e/ecs] Test Fargate
    Signed-off-by: Wassim DHIF <wassim.dhif@datadoghq.com>

--- test/new-e2e/tests/containers/ecs_test.go
+++ test/new-e2e/tests/containers/ecs_test.go
@@ -167,7 +167,7 @@ func (suite *ecsSuite) Test00UpAndRunning() {
 	})
 }
 
-func (suite *ecsSuite) TestNginx() {
+func (suite *ecsSuite) TestNginxECS() {
 	// `nginx` check is configured via docker labels
 	// Test it is properly scheduled
 	suite.testMetric(&testMetricArgs{
@@ -225,7 +225,7 @@ func (suite *ecsSuite) TestNginx() {
 	})
 }
 
-func (suite *ecsSuite) TestRedis() {
+func (suite *ecsSuite) TestRedisECS() {
 	// `redis` check is auto-configured due to image name
 	// Test it is properly scheduled
 	suite.testMetric(&testMetricArgs{
@@ -274,6 +274,120 @@ func (suite *ecsSuite) TestRedis() {
 				`^task_arn:arn:`,
 				`^task_family:.*-redis-ec2$`,
 				`^task_name:.*-redis-ec2$`,
+				`^task_version:[[:digit:]]+$`,
+			},
+			Message: `oO0OoO0OoO0Oo Redis is starting oO0OoO0OoO0Oo`,
+		},
+	})
+}
+
+func (suite *ecsSuite) TestNginxFargate() {
+	// `nginx` check is configured via docker labels
+	// Test it is properly scheduled
+	suite.testMetric(&testMetricArgs{
+		Filter: testMetricFilterArgs{
+			Name: "nginx.net.request_per_s",
+		},
+		Expect: testMetricExpectArgs{
+			Tags: &[]string{
+				`^cluster_name:` + regexp.QuoteMeta(suite.ecsClusterName) + `$`,
+				`^container_id:`,
+				`^container_name:ecs-.*-nginx-fg-`,
+				`^docker_image:ghcr.io/datadog/apps-nginx-server:main$`,
+				`^ecs_cluster_name:` + regexp.QuoteMeta(suite.ecsClusterName) + `$`,
+				`^ecs_container_name:nginx$`,
+				`^git.commit.sha:`,                                                       // org.opencontainers.image.revision docker image label
+				`^git.repository_url:https://github.com/DataDog/test-infra-definitions$`, // org.opencontainers.image.source   docker image label
+				`^image_id:sha256:`,
+				`^image_name:ghcr.io/datadog/apps-nginx-server$`,
+				`^image_tag:main$`,
+				`^nginx_cluster_name:` + regexp.QuoteMeta(suite.ecsClusterName) + `$`,
+				`^short_image:apps-nginx-server$`,
+				`^task_arn:`,
+				`^task_family:.*-nginx-fg*`,
+				`^task_name:.*-nginx-fg*`,
+				`^task_version:[[:digit:]]+$`,
+			},
+		},
+	})
+
+	suite.testLog(&testLogArgs{
+		Filter: testLogFilterArgs{
+			Service: "apps-nginx-server",
+		},
+		Expect: testLogExpectArgs{
+			Tags: &[]string{
+				`^cluster_name:` + regexp.QuoteMeta(suite.ecsClusterName) + `$`,
+				`^container_id:`,
+				`^container_name:ecs-.*-nginx-fg-`,
+				`^docker_image:ghcr.io/datadog/apps-nginx-server:main$`,
+				`^ecs_cluster_name:` + regexp.QuoteMeta(suite.ecsClusterName) + `$`,
+				`^ecs_container_name:nginx$`,
+				`^git.commit.sha:`,                                                       // org.opencontainers.image.revision docker image label
+				`^git.repository_url:https://github.com/DataDog/test-infra-definitions$`, // org.opencontainers.image.source   docker image label
+				`^image_id:sha256:`,
+				`^image_name:ghcr.io/datadog/apps-nginx-server$`,
+				`^image_tag:main$`,
+				`^short_image:apps-nginx-server$`,
+				`^task_arn:arn:`,
+				`^task_family:.*-nginx-fg*`,
+				`^task_name:.*-nginx-fg*`,
+				`^task_version:[[:digit:]]+$`,
+			},
+			Message: `GET / HTTP/1\.1`,
+		},
+	})
+}
+
+func (suite *ecsSuite) TestRedisFargate() {
+	// `redis` check is auto-configured due to image name
+	// Test it is properly scheduled
+	suite.testMetric(&testMetricArgs{
+		Filter: testMetricFilterArgs{
+			Name: "redis.net.instantaneous_ops_per_sec",
+		},
+		Expect: testMetricExpectArgs{
+			Tags: &[]string{
+				`^cluster_name:` + regexp.QuoteMeta(suite.ecsClusterName) + `$`,
+				`^container_id:`,
+				`^container_name:ecs-.*-redis-fg-`,
+				`^docker_image:redis:latest$`,
+				`^ecs_cluster_name:` + regexp.QuoteMeta(suite.ecsClusterName) + `$`,
+				`^ecs_container_name:redis$`,
+				`^image_id:sha256:`,
+				`^image_name:redis$`,
+				`^image_tag:latest$`,
+				`^redis_host:`,
+				`^redis_port:6379$`,
+				`^redis_role:master$`,
+				`^short_image:redis$`,
+				`^task_arn:`,
+				`^task_family:.*-redis-fg*`,
+				`^task_name:.*-redis-fg*`,
+				`^task_version:[[:digit:]]+$`,
+			},
+		},
+	})
+
+	suite.testLog(&testLogArgs{
+		Filter: testLogFilterArgs{
+			Service: "redis",
+		},
+		Expect: testLogExpectArgs{
+			Tags: &[]string{
+				`^cluster_name:` + regexp.QuoteMeta(suite.ecsClusterName) + `$`,
+				`^container_id:`,
+				`^container_name:ecs-.*-redis-fg-`,
+				`^docker_image:redis:latest$`,
+				`^ecs_cluster_name:` + regexp.QuoteMeta(suite.ecsClusterName) + `$`,
+				`^ecs_container_name:redis$`,
+				`^image_id:sha256:`,
+				`^image_name:redis$`,
+				`^image_tag:latest$`,
+				`^short_image:redis$`,
+				`^task_arn:arn:`,
+				`^task_family:.*-redis-fg*`,
+				`^task_name:.*-redis-fg*`,
 				`^task_version:[[:digit:]]+$`,
 			},
 			Message: `oO0OoO0OoO0Oo Redis is starting oO0OoO0OoO0Oo`,
