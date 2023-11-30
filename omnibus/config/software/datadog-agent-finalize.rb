72230f23d9ea0df3424b91fd54027a4f8425f502
Sylvain Baubeau <lebauce@gmail.com>
2023-11-30 18:42:57 +0100

    Add systemd files to start and stop system-probe and security-agent

--- omnibus/config/software/datadog-agent-finalize.rb
+++ omnibus/config/software/datadog-agent-finalize.rb
@@ -120,7 +120,13 @@ build do
             move "#{install_dir}/scripts/datadog-agent-trace.service", systemd_directory
             move "#{install_dir}/scripts/datadog-agent-process.service", systemd_directory
             move "#{install_dir}/scripts/datadog-agent-sysprobe.service", systemd_directory
+            move "#{install_dir}/scripts/datadog-agent-sysprobe.path", systemd_directory
+            move "#{install_dir}/scripts/datadog-agent-sysprobe-stop.service", systemd_directory
+            move "#{install_dir}/scripts/datadog-agent-sysprobe-stop.path", systemd_directory
             move "#{install_dir}/scripts/datadog-agent-security.service", systemd_directory
+            move "#{install_dir}/scripts/datadog-agent-security.path", systemd_directory
+            move "#{install_dir}/scripts/datadog-agent-security-stop.path", systemd_directory
+            move "#{install_dir}/scripts/datadog-agent-security.path", systemd_directory
 
             # Move configuration files
             mkdir "/etc/datadog-agent"
@@ -131,6 +137,7 @@ build do
             move "#{install_dir}/etc/datadog-agent/runtime-security.d", "/etc/datadog-agent", :force=>true
             move "#{install_dir}/etc/datadog-agent/security-agent.yaml.example", "/etc/datadog-agent", :force=>true
             move "#{install_dir}/etc/datadog-agent/compliance.d", "/etc/datadog-agent"
+            mkdir "/etc/datadog-agent/enabled.d"
 
             # Move SELinux policy
             if debian_target? || redhat_target?
